d5957d02e624d56ed48ef28cd4e8ac3b278c9ed9
QZJGeorge <zhijieq@umich.edu>
2023-09-29 14:35:00 -0400

    update terasim

---
+++ src/terasim/include/sumo_autoware_cosim.hpp
@@ -0,0 +1,93 @@
+//  Copyright 2022 Tier IV, Inc. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#ifndef MCITY_TERASIM__SumoAutowareCosim_HPP_
+#define MCITY_TERASIM__SumoAutowareCosim_HPP_
+
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+#include <hiredis/hiredis.h>
+#include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <autoware_adapi_v1_msgs/srv/clear_route.hpp>
+#include <autoware_adapi_v1_msgs/msg/route_state.hpp>
+#include <autoware_adapi_v1_msgs/srv/set_route_points.hpp>
+#include <autoware_adapi_v1_msgs/msg/operation_mode_state.hpp>
+#include <autoware_adapi_v1_msgs/srv/initialize_localization.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+
+namespace sumo_autoware_cosim
+{
+
+using namespace std;
+
+using geometry_msgs::msg::Pose;
+using geometry_msgs::msg::PoseWithCovarianceStamped;
+
+using autoware_adapi_v1_msgs::msg::RouteState;
+using autoware_adapi_v1_msgs::msg::OperationModeState;
+using autoware_adapi_v1_msgs::srv::ClearRoute;
+using autoware_adapi_v1_msgs::srv::SetRoutePoints;
+
+class SumoAutowareCosim : public rclcpp::Node
+{
+public:
+  explicit SumoAutowareCosim(const rclcpp::NodeOptions & options);
+  ~SumoAutowareCosim() = default;
+
+private:
+  const uint16_t UNKNOWN = 0;
+  const uint16_t UNSET = 1;
+  const uint16_t SET = 2;
+  const uint16_t ARRIVED = 3;
+  const uint16_t CHANGING = 4;
+
+  Pose wp0, wp1, wp2, wp3, wp4;
+
+  redisContext *context;
+
+  PoseWithCovarianceStamped localization_msg;
+
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  RouteState route_state;
+
+  rclcpp::Publisher<PoseWithCovarianceStamped>::SharedPtr pub_local;
+
+  rclcpp::Subscription<RouteState>::SharedPtr sub_route_state;
+  rclcpp::Subscription<OperationModeState>::SharedPtr sub_mode_state;
+
+  rclcpp::Client<ClearRoute>::SharedPtr cli_clear_route;
+  rclcpp::Client<SetRoutePoints>::SharedPtr cli_set_route_points;
+
+  void on_timer();
+  void init_redis_client();
+
+  void init_localization();
+  void pub_localization();
+
+  void init_route_points();
+  void set_route_points();
+
+  void clear_route();
+
+  void route_state_callback(RouteState::SharedPtr msg);
+  void mode_state_callback(OperationModeState::SharedPtr msg);
+
+  string get_key(string key);
+};
+
+}
+
+#endif
---
+++ src/terasim/src/sumo_autoware_cosim.cpp
@@ -0,0 +1,197 @@
+// Copyright 2019 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "sumo_autoware_cosim.hpp"
+
+namespace sumo_autoware_cosim{
+
+  SumoAutowareCosim::SumoAutowareCosim(const rclcpp::NodeOptions & options)
+  : Node("sumo_autoware_cosim", options)
+  {
+    pub_local = this->create_publisher<PoseWithCovarianceStamped>("/initialpose", 10);
+
+    cli_clear_route = this->create_client<ClearRoute>("/planning/mission_planning/clear_route");
+    cli_set_route_points = this->create_client<SetRoutePoints>("/planning/mission_planning/set_route_points");
+
+    sub_route_state = this->create_subscription<RouteState>(
+      "/planning/mission_planning/route_state", 10, std::bind(&SumoAutowareCosim::route_state_callback, this, std::placeholders::_1));
+
+    sub_mode_state = this->create_subscription<OperationModeState>(
+      "/system/operation_mode/state", 10, std::bind(&SumoAutowareCosim::mode_state_callback, this, std::placeholders::_1));
+
+    timer_ = rclcpp::create_timer(
+      this, get_clock(), 1000ms, std::bind(&SumoAutowareCosim::on_timer, this));
+
+    init_redis_client();
+    init_localization();
+    init_route_points();
+  }
+
+  void SumoAutowareCosim::on_timer(){
+    string terasim_state = get_key("terasim_state");
+    int state = stoi(terasim_state);
+
+    if (state == 0){
+      if (route_state.state != UNSET){
+        clear_route();
+      }
+      pub_localization();
+    } else{
+      if (route_state.state != SET){
+        set_route_points();
+      }
+    }
+  }
+
+  void SumoAutowareCosim::init_redis_client(){
+    // Connecting to the Redis server on localhost
+    context = redisConnect("127.0.0.1", 6379);
+
+    // handle error
+    if (context == NULL || context->err) {
+      if (context){
+        cout << "Error: " << context->errstr << endl;
+      } else {
+        std::cout << "Can't allocate redis context" << endl;
+      }
+    } else {
+      cout << "Connected to redis server at 127.0.0.1:6379" << endl;
+    }
+  }
+
+  string SumoAutowareCosim::get_key(string key){
+    // GET key
+    redisReply *reply = (redisReply *)redisCommand(context, "GET %s", key.c_str());
+    string result = "";
+    if (reply->type == REDIS_REPLY_STRING)
+      result = reply->str;
+    return result;
+  }
+
+  void SumoAutowareCosim::init_localization(){
+    localization_msg.pose.pose.position.x = 77603.84375;
+    localization_msg.pose.pose.position.y = 86526.0625;
+
+    localization_msg.pose.pose.orientation.x = 0.0;
+    localization_msg.pose.pose.orientation.y = 0.0;
+    localization_msg.pose.pose.orientation.z = 0.18735418183937574;
+    localization_msg.pose.pose.orientation.w = 0.9822924261885043;
+  }
+
+  void SumoAutowareCosim::init_route_points(){
+    wp0.position.x = 77656.75;
+    wp0.position.y = 86778.1953125;
+    wp0.position.z = 0.0;
+
+    wp0.orientation.x = 0.0;
+    wp0.orientation.y = 0.0;
+    wp0.orientation.z = 0.6845353431369396;
+    wp0.orientation.w = 0.7289796732395165;
+
+    wp1.position.x = 77616.9609375;
+    wp1.position.y = 86880.828125;
+    wp1.position.z = 0.0;
+
+    wp1.orientation.x = 0.0;
+    wp1.orientation.y = 0.0;
+    wp1.orientation.z = -0.7423686519969227;
+    wp1.orientation.w = 0.6699916301956853;
+
+    wp2.position.x = 77549.984375;
+    wp2.position.y = 86745.5546875;
+    wp2.position.z = 0.0;
+
+    wp2.orientation.x = 0.0;
+    wp2.orientation.y = 0.0;
+    wp2.orientation.z = -0.7337657020938361;
+    wp2.orientation.w = 0.6794026011362775;
+
+    wp3.position.x = 77581.4375;
+    wp3.position.y = 86651.9765625;
+    wp3.position.z = 0.0;
+
+    wp3.orientation.x = 0.0;
+    wp3.orientation.y = 0.0;
+    wp3.orientation.z = -0.015441629002764135;
+    wp3.orientation.w = 0.999880770939086;
+
+    wp4.position.x = 77538.734375;
+    wp4.position.y = 86518.5390625;
+    wp4.position.z = 0.0;
+
+    wp4.orientation.x = 0.0;
+    wp4.orientation.y = 0.0;
+    wp4.orientation.z = 0.06457478852556715;
+    wp4.orientation.w = 0.9979128702882223;
+  }
+
+  void SumoAutowareCosim::pub_localization(){
+    localization_msg.header.stamp = this->get_clock()->now();
+    localization_msg.header.frame_id = "map";
+    pub_local->publish(localization_msg);
+  }
+
+  void SumoAutowareCosim::clear_route(){
+    while (!cli_clear_route->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+      }
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "routing service not available, waiting again...");
+    }
+
+    auto req = std::make_shared<ClearRoute::Request>();
+    auto result_c =cli_clear_route->async_send_request(req);
+    cout << "clearing existing route..." << endl;
+  }
+
+  void SumoAutowareCosim::set_route_points(){
+    while (!cli_set_route_points->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+      }
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "routing service not available, waiting again...");
+    }
+
+    auto set_route_points_req = std::make_shared<SetRoutePoints::Request>();
+
+    set_route_points_req->header.frame_id = "map";
+    set_route_points_req->goal = wp4;
+    set_route_points_req->waypoints = {wp0, wp1, wp2, wp3};
+
+    auto result_s = cli_set_route_points->async_send_request(set_route_points_req);
+  }
+
+  void SumoAutowareCosim::route_state_callback(RouteState::SharedPtr msg){
+    route_state = *msg;
+    auto state = route_state.state;
+
+    if (state == UNKNOWN){
+      cout << "route state: unknown" << endl;
+    } else if (state == UNSET){
+      cout << "route state: unset" << endl;
+    } else if (state == SET){
+      cout << "route state: set" << endl;
+    } else if (state == ARRIVED){
+      cout << "route state: arrived" << endl;
+    } else if (state == CHANGING){
+      cout << "route state: changing" << endl;
+    }
+  }
+
+  void SumoAutowareCosim::mode_state_callback(OperationModeState::SharedPtr msg){
+      cout << "current operation mode: " << (int)msg->mode << endl;
+  }
+}
+
+RCLCPP_COMPONENTS_REGISTER_NODE(sumo_autoware_cosim::SumoAutowareCosim)
