8d4a636b2acd6b1e8f8624aa2d18f91a99c58a0b
QZJGeorge <zhijieq@umich.edu>
2023-10-02 15:29:10 -0400

    modify launch files

---
+++ src/terasim/launch/cosim.launch.py
@@ -0,0 +1,61 @@
+import os
+import redis
+import launch
+
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, Shutdown, IncludeLaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        Node(
+            package='mcity_terasim',
+            namespace='/terasim',
+            executable='cav_info_converter',
+        ),
+        Node(
+            package='mcity_terasim',
+            namespace='/terasim',
+            executable='cav_context_converter',
+        ),
+        Node(
+            package='mcity_terasim',
+            namespace='/terasim',
+            executable='traffic_signal_control',
+        ),
+        Node(
+            package='mcity_terasim',
+            namespace='/terasim',
+            executable='sumo_autoware_cosim',
+        ),
+        DeclareLaunchArgument(
+            'map_path',
+            default_value=str(os.getenv('HOME')) + '/autoware/map',
+            description='Path to map file'
+        ),
+        DeclareLaunchArgument(
+            'vehicle_model', 
+            default_value='sample_vehicle',
+            description='Vehicle model'
+        ),
+        DeclareLaunchArgument(
+            'sensor_model',
+            default_value='sample_sensor_kit',
+            description='Sensor model'
+        ),
+        DeclareLaunchArgument(
+            'lanelet2_map_file',
+            default_value='lanelet2_mcity_v11.osm',
+            description='Lanelet2 map file'
+        ),
+        IncludeLaunchDescription(
+            AnyLaunchDescriptionSource(str(os.getenv('HOME')) + '/autoware/src/launcher/autoware_launch/autoware_launch/launch/planning_simulator.launch.xml'),
+            launch_arguments={
+                'map_path': LaunchConfiguration('map_path'),
+                'vehicle_model': LaunchConfiguration('vehicle_model'),
+                'sensor_model': LaunchConfiguration('sensor_model'),
+                'lanelet2_map_file': LaunchConfiguration('lanelet2_map_file')
+            }.items()
+        ),
+    ])
