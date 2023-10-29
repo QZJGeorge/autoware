8728c06e2136a17704cfcb5c20104b8ee1f262b5
QZJGeorge <zhijieq@umich.edu>
2023-10-29 11:33:55 -0400

    update launch file to map v25

--- src/terasim/mcity_terasim/launch/cosim.launch.py
+++ src/terasim/mcity_terasim/launch/cosim.launch.py
@@ -44,7 +44,7 @@ def generate_launch_description():
         ),
         DeclareLaunchArgument(
             'lanelet2_map_file',
-            default_value='lanelet2_mcity_v24.osm',
+            default_value='lanelet2_mcity_v25.osm',
             description='Lanelet2 map file'
         ),
         IncludeLaunchDescription(
