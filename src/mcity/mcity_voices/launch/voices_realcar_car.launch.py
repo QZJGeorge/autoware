a17f6d58a1bea058cf6d5fd51bfadabebef19ea5
QZJGeorge <zhijieq@umich.edu>
2024-03-12 18:15:13 -0400

    add mcity voices remote testing capability

---
+++ src/mcity/mcity_voices/launch/voices_realcar_car.launch.py
@@ -0,0 +1,42 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    return LaunchDescription([
+
+        ############################################################
+        # Localization (GNSS to Autoware to SUMO)
+        ############################################################
+        Node(
+            package='mcity_localization',
+            namespace='/mcity/localization',
+            executable='gnss_to_autoware',
+        ),
+        
+        ############################################################
+        # Control (Preview Control & Drive by Wire)
+        ############################################################
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                os.path.join(get_package_share_directory('mcity_control'),'launch','mcity_control.launch.py')
+            ])
+        ),
+
+        ############################################################
+        # Remote Communication
+        ############################################################
+        Node(
+            package='mcity_remote_com',
+            namespace='/mcity/communication',
+            executable='ros_to_redis_vehicle_state',
+        ),
+        Node(
+            package='mcity_remote_com',
+            namespace='/mcity/communication',
+            executable='redis_to_ros_input_path',
+        ),
+    ])
---
+++ src/mcity/mcity_voices/launch/voices_realcar_user.launch.py
@@ -0,0 +1,71 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+
+        ############################################################
+        # Autoware Interface to handle the simulation requests
+        ############################################################
+        Node(
+            package='mcity_voices',
+            namespace='/mcity/voices',
+            executable='autoware_interface_voices_realcar',
+        ),
+
+        ############################################################
+        # Localization (GNSS to Autoware to SUMO)
+        ############################################################
+        Node(
+            package='mcity_localization',
+            namespace='/mcity/localization',
+            executable='gnss_to_autoware',
+        ),
+        Node(
+            package='mcity_localization',
+            namespace='/mcity/localization',
+            executable='autoware_to_sumo',
+        ),
+        
+        ############################################################
+        # Perception (SUMO to Autoware)
+        ############################################################
+        Node(
+            package='mcity_perception',
+            namespace='/mcity/perception',
+            executable='sumo_background_vehicle_converter',
+        ),
+        Node(
+            package='mcity_perception',
+            namespace='/mcity/perception',
+            executable='sumo_traffic_light_converter',
+        ),
+        Node(
+            package='mcity_perception',
+            namespace='/mcity/perception',
+            executable='sumo_occ_grid_converter',
+        ),
+
+        ############################################################
+        # Planning (Autoware Path to Preview Control)
+        ############################################################
+        Node(
+            package='mcity_planning',
+            namespace='/mcity/planning',
+            executable='autoware_path',
+        ),
+        
+        ############################################################
+        # Remote Communication
+        ############################################################
+        Node(
+            package='mcity_remote_com',
+            namespace='/mcity/communication',
+            executable='redis_to_ros_vehicle_state',
+        ),
+        Node(
+            package='mcity_remote_com',
+            namespace='/mcity/communication',
+            executable='ros_to_redis_input_path',
+        ),
+    ])
