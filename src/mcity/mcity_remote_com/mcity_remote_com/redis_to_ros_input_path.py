6abb6b12a95cb3ed28cda9c2a0e3018ccd12ca5e
QZJGeorge <zhijieq@umich.edu>
2024-03-14 16:14:56 -0400

    working version of computing planning latency

--- src/mcity/mcity_remote_com/mcity_remote_com/redis_to_ros_input_path.py
+++ src/mcity/mcity_remote_com/mcity_remote_com/redis_to_ros_input_path.py
@@ -17,37 +17,20 @@ class RedisToRosInputPath(BasicRosRedisComNode):
         json_str = self.redis_client.get(constants.PLANNED_PATH)
         if json_str is not None:
             ros_message_dict  = json.loads(json_str)
+            self.set_redis_latency(ros_message_dict)
+
             msg = PlannedPath()
             message_conversion.populate_instance(ros_message_dict, msg)
             self.publisher_planned_path.publish(msg)
 
-            latency = self.calc_planning_latency(ros_message_dict)
+    def set_redis_latency(self, ros_message_dict):
+        current_time = self.get_clock().now().seconds_nanoseconds()
+        current_timestamp = current_time[0] + current_time[1] * 1e-9
 
-            if latency is None:
-                self.redis_client.set(constants.AV_PLANNING_LATENCY, 0.0)
-            else:
-                self.redis_client.set(constants.AV_PLANNING_LATENCY, latency)
+        latency = current_timestamp - ros_message_dict['timestamp']
 
-    def get_current_time(self):
-        # Assuming self.node is your ROS 2 node instance
-        now = self.get_clock().now()
-        # Convert ROS 2 time to seconds with nanoseconds as fractional part
-        current_time = now.seconds_nanoseconds()
-        return current_time[0] + current_time[1] * 1e-9
+        self.redis_client.set(constants.AV_PLANNING_LATENCY, latency)
 
-    def calc_planning_latency(self, ros_message_dict):
-        if 'header' in ros_message_dict:
-            header = ros_message_dict['header']
-            if 'stamp' in header:
-                stamp = header['stamp']
-                if 'secs' in stamp and 'nsecs' in stamp:
-                    # Convert ROS message time to seconds with nanoseconds as fractional part
-                    message_time = stamp['secs'] + stamp['nsecs'] * 1e-9
-                    # Calculate latency by subtracting message time from current time
-                    return self.get_current_time() - message_time
-
-        return None
-        
 def main(args=None):
     rclpy.init(args=args)
     node = RedisToRosInputPath()
