5279a9c408e93d20e9af4093f49b327dffce9af8
QZJGeorge <zhijieq@umich.edu>
2024-04-03 13:59:13 -0400

    modify abc_realcar launch file

--- src/mcity/mcity_abc_realcar/launch/mcity_abc_realcar.launch.py
+++ src/mcity/mcity_abc_realcar/launch/mcity_abc_realcar.launch.py
@@ -1,7 +1,11 @@
+import os
 import launch
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     return launch.LaunchDescription([
@@ -19,6 +23,16 @@ def generate_launch_description():
             namespace='/mcity',
             executable=LaunchConfiguration('scenario')
         ),
+
+        ############################################################
+        # Localization
+        ############################################################
+        Node(
+            package='mcity_localization',
+            namespace='/mcity/localization',
+            executable='mcity_localization',
+        ),
+
         ############################################################
         # Mixed Reality
         ############################################################
@@ -27,20 +41,37 @@ def generate_launch_description():
             namespace='/mcity',
             executable='cav_state_converter',
         ),
-
         Node(
             package='mcity_mr',
             namespace='/mcity',
             executable='cav_context_converter',
-        ),
-        Node(
-            package='mcity_mr',
-            namespace='/mcity',
-            executable='occ_grid_converter',
         ),
         # Node(
         #     package='mcity_mr',
         #     namespace='/mcity',
         #     executable='sumo_light_converter',
         # ),
+        Node(
+            package='mcity_mr',
+            namespace='/mcity',
+            executable='occ_grid_converter',
+        ),
+
+        ############################################################
+        # Planning
+        ############################################################
+        Node(
+            package='mcity_planning',
+            namespace='/mcity/planning',
+            executable='autoware_path',
+        ),
+        
+        ############################################################
+        # Control
+        ############################################################
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                os.path.join(get_package_share_directory('mcity_control'),'launch','mcity_control.launch.py')
+            ])
+        ),
     ])
